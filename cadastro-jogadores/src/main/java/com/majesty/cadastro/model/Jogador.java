35b489afe9ec618e2faf080d51a82952bc118a36
“[jacob-majesty]” <[jacobamony@gmail.com]>
2024-12-12 10:51:01 -0300

    Criei o schema e app properties

--- cadastro-jogadores/src/main/java/com/majesty/cadastro/model/Jogador.java
+++
@@ -1,5 +0,0 @@
-package com.majesty.cadastro.model;
-
-public record Jogador() {
-
-}
---
+++ cadastro/src/main/java/com/majesty/cadastro/model/Jogador.java
@@ -0,0 +1,6 @@
+package com.majesty.cadastro.model;
+
+public record Jogador(String nome, String email, String telefone, String codinome, GrupoCodinome grupoCodinome) {
+
+
+}
